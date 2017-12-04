dd15ac6326119098e76b4a98d2c50d4ab49221b5
Kyle <31713318+GHAngeloid@users.noreply.github.com>
2017-12-04 03:33:12 -0500

    Add files via upload

--- asst/views/stats.py
+++ asst/views/stats.py
@@ -8,9 +8,58 @@ import random
 import json
 import time
 
+import datetime
+
+print("TEST")
+
+class reser:
+    date1=datetime.date(2015, 1, 1)
+    date2=datetime.date(2018, 4, 5)
+    spent = 0
+    egg=0
+    bacon= 0
+    sandwich=0
+    cleaning = 0
+
+r1= reser()
+r2 = reser()
+r2.date1= datetime.date(2016, 6, 7)
+
+
+
+
+'''
+x=tst1.date2-tst1.date1
+y=x.days*2
+'''
+
+
+
 page = Blueprint('stats', __name__, template_folder='templates')
 
 @page.route("/",methods=['GET'])
 @require_role(['admin','manager'],getrole=True) # Example of requireing a role(and authentication)
 def stats(role):
+    return render_template('stats/index.html', logged_in=True,role=role)
+
+
+
+@page.route('/result',methods=['GET','POST'])
+@require_role(['admin','manager', 'customer'],getrole=True) # Example of requireing a role(and authentication)
+def result(role):
+    if request.method == 'POST':
+        res = request.form['res']
+        dateB = request.form['dateB']
+        dateE = request.form['dateE']
+        print(res)
+        print(dateB)
+        print(dateE)
+        if res == 'Highest Rated Room Type':
+            print(1)
+        if res == '5 Best Customers':
+            print(2)
+        if res == 'Highest Rated Breakfast':
+            print(3)
+        if res == 'Highest Rated Service':
+            print(4)
     return render_template('stats/index.html', logged_in=True,role=role)
