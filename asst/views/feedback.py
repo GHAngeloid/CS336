8310c1ac34dd75440862ad92f63d5ac4a49cf6f4
yuuriix <mashiroku@gmail.com>
2017-12-06 19:38:40 -0500

    Review type Selection
    Textboxes, hidden option, Select review type

--- asst/views/feedback.py
+++ asst/views/feedback.py
@@ -2,7 +2,9 @@ from flask import Blueprint, render_template, abort, flash
 from flask import request
 from asst.auth import require_role
 from itertools import *
-import traceback
+from asst import DB as db
+from asst.models import user
+import sys,traceback
 import math
 import random
 import json
@@ -14,3 +16,12 @@ page = Blueprint('feedback', __name__, template_folder='templates')
 @require_role(['admin','manager', 'customer'],getrole=True) # Example of requireing a role(and authentication)
 def feedback(role):
     return render_template('feedback/index.html', logged_in=True,role=role)
+def connect_to_db():
+    """Connect to the database before each request."""
+    try:
+        db.connect()
+        flash('Connected to db', 'success')
+    except:
+        traceback.print_exc(file=sys.stdout)
+        flash('Cannot connect to database', 'danger')
+        pass
