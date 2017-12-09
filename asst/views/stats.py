2c5547820ed505089c9ce9cffd9fdfd9d79a7463
Suva Shahria <1uknow2@gmail.com>
2017-12-08 20:17:30 -0500

    breakfast and services work

--- asst/views/stats.py
+++ asst/views/stats.py
@@ -11,45 +11,41 @@ import time
 
 import datetime
 
+
 # indicator that when starting up the site, it initializes every file, including possible global variables
-#print("TEST")
+# print("TEST")
 
 class reser:
-    date1=datetime.date(2015, 1, 1)
-    date2=datetime.date(2018, 4, 5)
+    date1 = datetime.date(2015, 1, 1)
+    date2 = datetime.date(2018, 4, 5)
     spent = 0
-    egg=0
-    bacon= 0
-    sandwich=0
+    egg = 0
+    bacon = 0
+    sandwich = 0
     cleaning = 0
 
-r1= reser()
+
+r1 = reser()
 r2 = reser()
-r2.date1= datetime.date(2016, 6, 7)
-
-
-
+r2.date1 = datetime.date(2016, 6, 7)
 
 '''
 x=tst1.date2-tst1.date1
 y=x.days*2
 '''
 
-
-
 page = Blueprint('stats', __name__, template_folder='templates')
 
-@page.route("/",methods=['GET'])
-@require_role(['admin','manager'],getrole=True) # Example of requireing a role(and authentication)
+
+@page.route("/", methods=['GET'])
+@require_role(['admin', 'manager'], getrole=True)  # Example of requireing a role(and authentication)
 def stats(role):
-    return render_template('stats/index.html', logged_in=True,role=role)
+    return render_template('stats/index.html', logged_in=True, role=role)
 
 
-
-@page.route('/stat_list',methods=['GET','POST'])
-@require_role(['admin','manager', 'customer'],getrole=True) # Example of requireing a role(and authentication)
+@page.route('/stat_list', methods=['GET', 'POST'])
+@require_role(['admin', 'manager', 'customer'], getrole=True)  # Example of requireing a role(and authentication)
 def result(role):
-
     if request.method == 'GET':
         return render_template('stats/stat_list.html', logged_in=True, role=role)
     if request.method == 'POST':
@@ -57,15 +53,15 @@ def result(role):
         dateB = request.form['dateB']
         dateE = request.form['dateE']
         print(result)
-        #print(dateB)
-        #print(dateE)
+        # print(dateB)
+        # print(dateE)
 
         # Highest Rated Room Type
         if result == 'Highest Rated Room Type':
             hotels = []
             try:
                 for r in res.Reservation.select().where(res.Reservation.InDate >= dateB
-                and res.Reservation.OutDate <= dateE):
+                                                        and res.Reservation.OutDate <= dateE):
                     if r.HotelID not in hotels:
                         hotels.append(r.HotelID)
 
@@ -75,22 +71,21 @@ def result(role):
                 k = 0
                 while i < len(hotels):
                     for r in roomreview_evaluates.RoomReview_evaluates.select().where(
-                    roomreview_evaluates.RoomReview_evaluates.HotelID == hotels[i]):
+                            roomreview_evaluates.RoomReview_evaluates.HotelID == hotels[i]):
                         print("Review ID:", r.ReviewID, "Room_no:", r.Room_no)
-                        if(r.Room_no not in Room_no_list):
+                        if (r.Room_no not in Room_no_list):
                             Room_no_list.append(r.Room_no)
                             reviewcnt.append(1)
                             k += 1
-                        #else:
+                        # else:
 
                     i += 1
 
                 for r in room.Room.select().where(
-                room.Room.HotelID == hotels[0] and room.Room.Room_no == Room_no_list[0]):
+                        room.Room.HotelID == hotels[0] and room.Room.Room_no == Room_no_list[0]):
                     print("Room Type:", r.Type)
 
-
-                #print(test)
+                # print(test)
             except Exception as e:
                 traceback.print_exc(file=sys.stdout)
                 return "Error", 500
@@ -106,8 +101,8 @@ def result(role):
                 TotalAmountArray = []
                 i = 0
                 for r in res.Reservation.select().where(res.Reservation.InDate >= dateB
-                and res.Reservation.OutDate <= dateE):
-                    #print(temp, r.TotalAmt)
+                                                        and res.Reservation.OutDate <= dateE):
+                    # print(temp, r.TotalAmt)
 
                     if r.CID in CIDArray:
                         key = CIDArray.index(r.CID)
@@ -117,11 +112,11 @@ def result(role):
                         TotalAmountArray.append(r.TotalAmt)
                         i += 1
 
-                #print(CIDArray, TotalAmountArray)
+                # print(CIDArray, TotalAmountArray)
                 sortedList = sorted(TotalAmountArray, reverse=True)
                 i = 0  # sorted index
                 newCIDArray = []
-                #print(sortedList)
+                # print(sortedList)
 
                 while i < len(CIDArray):
                     j = 0  # unsorted index
@@ -131,7 +126,7 @@ def result(role):
                     if len(newCIDArray) == 5:
                         break
                     i += 1
-                #print(newCIDArray, sortedList)
+                # print(newCIDArray, sortedList)
 
                 i = 0
                 name = []
@@ -157,24 +152,103 @@ def result(role):
 
         # Highest Rated Breakfast
         if result == 'Highest Rated Breakfast':
+
             hotels = []  # list of every hotel
+            cids = []
             try:
                 for r in res.Reservation.select().where(res.Reservation.InDate >= dateB
-                and res.Reservation.OutDate <= dateE):
+                                                        and res.Reservation.OutDate <= dateE):
                     if r.HotelID not in hotels:
                         hotels.append(r.HotelID)
+                    if r.CID not in cids:
+                        cids.append(r.CID)
 
                 i = 0
-                btypes = []  # list of all breakfast types per hotel
+                reviewIDs = []  # list of all breakfast types per hotel
                 while i < len(hotels):  # iterates through each hotel
                     for b in breakfastreview_asseses.BreakfastReview_asseses.select().where(
-                    breakfastreview_asseses.BreakfastReview_asseses.HotelID == hotels[i]):
-                        print("Review ID:", b.ReviewID, "Breakfast Type:", b.BType)
-                        if b.BType not in btypes:
-                            btypes.append(b.BType)
+                            breakfastreview_asseses.BreakfastReview_asseses.HotelID == hotels[i]):
+                        #  print("Review ID:", b.ReviewID, "Breakfast Type:", b.BType)
+                        if b.ReviewID not in reviewIDs:
+                            reviewIDs.append(b.ReviewID)
                     i += 1
 
+                # print(reviewIDs)
+                #find
 
+                revID = []
+                revR = []
+                i = 0
+                while i < len(reviewIDs):
+
+                    for u in review.review.select().where(review.review.ReviewID == reviewIDs[i]):
+                        revID.append(u.ReviewID)
+                        revR.append(u.Rating)
+                    i += 1
+
+                af = []
+                bf = []
+                cf = []
+                mf = []
+                i = 0
+                while i < len(revID):
+                    for m in breakfastreview_asseses.BreakfastReview_asseses.select().where(
+                            breakfastreview_asseses.BreakfastReview_asseses.ReviewID == revID[i]):
+                        if m.BType == "Bengali":
+                            bf.append(revR[i])
+                        if m.BType == "American":
+                            af.append(revR[i])
+                        if m.BType == "Chinese":
+                            cf.append(revR[i])
+                        if m.BType == "Mexican":
+                            mf.append(revR[i])
+
+
+                    i += 1
+                a = 0
+                b = 0
+                c = 0
+                m = 0
+                if len(af) != 0:
+                    a = sum(af)/len(af)
+
+                if len(bf) != 0:
+                    b = sum(bf)/len(bf)
+
+                if len(cf) != 0:
+                    c = sum(cf)/len(cf)
+
+                if len(mf) != 0:
+                    m = sum(mf)/len(mf)
+
+                sol = []
+                sol.append(a)
+                sol.append(b)
+                sol.append(c)
+                sol.append(m)
+
+                sol2 = sorted(sol, reverse=True)
+                print(sol, sol2)
+
+                output = 'hi'
+
+                if sol2[0] == a:
+                    output = 'American'
+
+                if sol2[0] == b:
+                    output = 'Bengali'
+
+                if sol2[0] == c:
+                    output = 'Chinese'
+
+                if sol2[0] == m:
+                    output = 'Mexican'
+
+                return render_template('stats/stat_var.html', logged_in=True, role=role, result=result,
+                                       output=output)
+
+
+            # qq
 
             except Exception as e:
                 traceback.print_exc(file=sys.stdout)
@@ -186,22 +260,94 @@ def result(role):
 
         # Highest Rated Services
         if result == 'Highest Rated Service':
-            hotels = []
+
+            hotels = []  # list of every hotel
+            cids = []
             try:
                 for r in res.Reservation.select().where(res.Reservation.InDate >= dateB
-                and res.Reservation.OutDate <= dateE):
+                                                        and res.Reservation.OutDate <= dateE):
                     if r.HotelID not in hotels:
                         hotels.append(r.HotelID)
+                    if r.CID not in cids:
+                        cids.append(r.CID)
 
                 i = 0
-                servicetypes = []  # list of all service types per hotel
-                while i < len(hotels):
-                    for s in servicereview_rates.ServiceReview_rates.select().where(
-                    servicereview_rates.ServiceReview_rates.HotelID == hotels[i]):
-                        print("Review ID:", s.ReviewID, "Service Type:", s.sType)
-                        if s.sType not in servicetypes:
-                            servicetypes.append(s.sType)
+                reviewIDs = []  # list of all breakfast types per hotel
+                while i < len(hotels):  # iterates through each hotel
+                    for b in servicereview_rates.ServiceReview_rates.select().where(
+                            servicereview_rates.ServiceReview_rates.HotelID == hotels[i]):
+                        #  print("Review ID:", b.ReviewID, "Breakfast Type:", b.BType)
+                        if b.ReviewID not in reviewIDs:
+                            reviewIDs.append(b.ReviewID)
                     i += 1
+
+                revID = []
+                revR = []
+                i = 0
+                while i < len(reviewIDs):
+
+                    for u in review.review.select().where(review.review.ReviewID == reviewIDs[i]):
+                        revID.append(u.ReviewID)
+                        revR.append(u.Rating)
+                    i += 1
+
+                cl = []
+                la = []
+                wi = []
+
+                i = 0
+                while i < len(revID):
+                    for m in servicereview_rates.ServiceReview_rates.select().where(
+                            servicereview_rates.ServiceReview_rates.ReviewID == revID[i]):
+                        if m.sType == "cleaning":
+                            cl.append(revR[i])
+                        if m.sType == "laundry":
+                            la.append(revR[i])
+                        if m.sType == "wifi":
+                            wi.append(revR[i])
+
+                    i += 1
+
+               # print(cl, la, wi)
+                c = 0
+                l = 0
+                w = 0
+
+                if len(cl) != 0:
+                    c = sum(cl)/len(cl)
+
+                if len(la) != 0:
+                    l = sum(la)/len(la)
+
+                if len(wi) != 0:
+                    w = sum(wi)/len(wi)
+
+
+                sol = []
+                sol.append(c)
+                sol.append(l)
+                sol.append(w)
+
+
+                sol2 = sorted(sol, reverse=True)
+                #print(sol, sol2)
+
+                output = 'hi'
+
+                if sol2[0] == c:
+                    output = 'Cleaning'
+
+                if sol2[0] == l:
+                    output = 'Laundry'
+
+                if sol2[0] == w:
+                    output = 'Wifi'
+
+                return render_template('stats/stat_var.html', logged_in=True, role=role, result=result,
+                                       output=output)
+
+
+
 
 
 
