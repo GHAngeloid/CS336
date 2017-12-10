b63ad10bdc0f867073c5efe0ea5bdf6c832ac6a1
yuuriix <mashiroku@gmail.com>
2017-12-10 16:43:19 -0500

    .....now just need to figure out why none is being sent to json

--- asst/views/feedback.py
+++ asst/views/feedback.py
@@ -15,6 +15,13 @@ import time
 
 page = Blueprint('feedback', __name__, template_folder='templates')
 inv_no = '1'
+rrate = '0'
+bfrate = '0'
+srate = '0'
+ReviewType = '0'
+rtype = 'none'
+bftype = 'none'
+stype = 'none'
 class ItemTable(Table):
     '''
     This is a itemTable class that generates text/html automatically to create a table for created reservations
@@ -57,6 +64,42 @@ def pick_res(role):
     table = ItemTable(reserv)
     return render_template('feedback/make_rev.html', logged_in=True,role=role, table = table)
 
+@page.route("/rating", methods=['POST'])
+@require_role(['admin','manager', 'customer'],getrole=True) # Example of requireing a role(and authentication)
+def get_rating_and_global(role):
+    global rrate
+    global bfrate
+    global srate
+    global ReviewType
+    global rtype
+    global bftype
+    global stype
+    hold = []
+    try:
+        data = request.get_json()
+        rate = data['radioValue']
+        RevType = data['RevType']
+        if(RevType == '1'):
+            rrate = rate
+        elif(RevType == '2'):
+            bfrate = rate
+        elif(RevType == '3'):
+            srate = rate
+        roomtype = data['rtype']
+        breaktype = data['bftype']
+        servtype = data['stype']
+        ReviewType = RevType
+        rtype = roomtype
+        bftype = breaktype
+        stype = servtype
+        print(str(servtype))
+        hold.append([RevType])
+        return json.dumps(hold)
+    except Exception as e:
+        traceback.print_exc(file=sys.stdout)
+        return "Error", 500
+
+
 @page.route("/load_Res", methods=['POST'])
 @require_role(['admin','manager', 'customer'],getrole=True) # Example of requireing a role(and authentication)
 def get_reserv(role):
@@ -88,67 +131,67 @@ def get_reserv(role):
 @require_role(['admin','manager', 'customer'],getrole=True) # Example of requireing a role(and authentication)
 def rreview(role):
     SCounter = 0
-    try:
-        user = flask_login.current_user
-        cid = user.CID
-        RevType = request.form['rest']
-        rrate = int(request.form['optradio'])
-        bfrate = int(request.form['optradio2'])
-        srate = int(request.form['optradio3'])
-        roomtype = request.form['rtype']
-        bftype = request.form['bftype']
-        stype = request.form['stype']
-        global inv_no
-        q = res.Reservation.select().where(res.Reservation.CID == cid, res.Reservation.InvoiceNo == inv_no)
-        if RevType == '1':
-            try:
-                rm = room.Room.select().where(room.Room.HotelID == q.HotelID, room.Room.Room_no == q.Room_no)
-                if rm.Type == roomtype:
-                    Text = request.form['description']
-                    review.Review.create_review(rrate, Text, cid, inv_no)
-                    SCounter = 1
-            except:
-                traceback.print_exc(file=sys.stdout)
-                flash("There was an error processing your request. Please try again", 'danger')
-                return render_template('feedback/index.html', logged_in=True, role=role)
-        elif RevType == '2':
-            try:
-                for food in includes.Inc_Breakfast.select().where(includes.Inc_Breakfast.InvoiceNo == inv_no):
+    global rate
+    global inv_no
+    print(str(inv_no))
+    global ReviewType
+    global rtype
+    global bftype
+    global stype
+    global rrate
+    global bfrate
+    global srate
+    print(str(ReviewType))
+    if SCounter == 0 :
+        try:
+            user = flask_login.current_user
+            cid = user.CID
+            for q in res.Reservation.select().where(res.Reservation.CID == cid, res.Reservation.InvoiceNo == inv_no):
+                if ReviewType == '1':
                     try:
-                        if food.BType == bftype:
-                            Text = request.form['description2']
-                            review.Review.create_review(bfrate, Text, cid, inv_no)
-                            SCounter = 1
+                        for rm in room.Room.select().where(room.Room.HotelID == q.HotelID, room.Room.Room_no == q.Room_no):
+                            if rm.Type == rtype:
+                                Text = request.form['description']
+                                review.Review.create_review(rrate, Text, cid, inv_no)
+                                SCounter = ReviewType
                     except:
-                        continue
-            except:
-                traceback.print_exc(file=sys.stdout)
-                flash("There was an error processing your request. Please try again", 'danger')
-                return render_template('feedback/index.html', logged_in=True, role=role)
-        elif RevType == '3':
-            try:
-                for ser in includes.Cont_Service.select().where(includes.Cont_Service.InvoiceNo == inv_no):
+                        traceback.print_exc(file=sys.stdout)
+                        flash("There was an error processing your request. Please try again", 'danger')
+                        return render_template('feedback/index.html', logged_in=True, role=role)
+                elif ReviewType == '2':
                     try:
-                        if ser.SType == stype:
-                            Text = request.form['description3']
-                            review.Review.create_review(srate, Text, cid, inv_no)
-                            SCounter = 1
+                        for food in includes.Inc_Breakfast.select().where(includes.Inc_Breakfast.InvoiceNo == inv_no):
+                            try:
+                                if food.BType == bftype:
+                                    Text = request.form['description2']
+                                    review.Review.create_review(bfrate, Text, cid, inv_no)
+                                    SCounter = ReviewType
+                            except:
+                                continue
                     except:
-                        continue
-            except:
-                traceback.print_exc(file=sys.stdout)
-                flash("There was an error processing your request. Please try again", 'danger')
-                return render_template('feedback/index.html', logged_in=True, role=role)
-    except:
-        traceback.print_exc(file=sys.stdout)
-        flash("There was an error processing your request. Please try again", 'danger')
-        return render_template('feedback/index.html', logged_in=True,role=role)
-    if SCounter != 0:
-        return render_template('feedback/success.html', logged_in=True, role=role)
-    else:
-        flash("error", 'danger')
-        return render_template('feedback/index.html',logged_in=True, role=role)
-@page.route('/success',methods=['GET', 'POST'])
-@require_role(['admin','manager', 'customer'],getrole=True) # Example of requireing a role(and authentication)
-def submit(role):
-    return render_template('feedback/success.html', logged_in=True, role=role)
+                        traceback.print_exc(file=sys.stdout)
+                        flash("There was an error processing your request. Please try again", 'danger')
+                        return render_template('feedback/index.html', logged_in=True, role=role)
+                elif ReviewType == '3':
+                    try:
+                        for ser in includes.Cont_Service.select().where(includes.Cont_Service.InvoiceNo == inv_no):
+                            try:
+                                if ser.SType == stype:
+                                    Text = request.form['description3']
+                                    review.Review.create_review(srate, Text, cid, inv_no)
+                                    SCounter = ReviewType
+                            except:
+                                continue
+                    except:
+                        traceback.print_exc(file=sys.stdout)
+                        flash("There was an error processing your request. Please try again", 'danger')
+                        return render_template('feedback/index.html', logged_in=True, role=role)
+        except:
+            traceback.print_exc(file=sys.stdout)
+            flash("There was an error processing your request. Please try again", 'danger')
+            return render_template('feedback/index.html', logged_in=True,role=role)
+        if SCounter == 0:
+            flash("error, user did not click the validate button", 'danger')
+            return render_template('feedback/index.html', logged_in=True, role=role)
+        else:
+            return render_template('feedback/success.html', logged_in=True, role=role)
