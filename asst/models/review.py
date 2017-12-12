90879bbec3109b0d6f9b8a9c6c922d4683afe4f1
Brandon S <HyperBCS@users.noreply.github.com>
2017-12-11 21:04:06 -0500

    Update review.py
    forgot to put review

--- asst/models/review.py
+++ asst/models/review.py
@@ -7,6 +7,18 @@ from asst.models import BaseModel
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash
 
+class review(UserMixin, BaseModel):
+    class Meta:
+        db_table = 'writes_review'
+    '''A User model for who will be using the software. Users have different levels of access with different roles
+    Current active roles:
+        - customer
+        - manager
+    '''
+    ReviewID = IntegerField(primary_key=True)
+    Rating = IntegerField()
+    TextComment = CharField()
+    CID = IntegerField()
 
 class writes_Review(UserMixin, BaseModel):
     class Meta:
